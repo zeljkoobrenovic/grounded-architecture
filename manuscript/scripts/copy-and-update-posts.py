b8b7fd23ee9dd8d76b9792a9cbde38a95538b93b
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2023-05-23 18:57:37 +0200

    publishing changes

--- manuscript/scripts/copy-and-update-posts.py
+++ manuscript/scripts/copy-and-update-posts.py
@@ -70,7 +70,7 @@ def copy_post(post):
 posts = ['2022-01-01-intro.markdown', '2022-01-02-context.markdown', '2022-01-03-requirements.markdown', '2022-03-01-grounded-architecture.markdown', '2022-03-03-data-platform.markdown', '2022-03-04-people.markdown',
          '2022-03-05-activities.markdown', '2022-03-09-value.markdown', '2022-06-01-being-architect.markdown', '2022-06-02-superglue.markdown', '2022-06-03-skills.markdown', '2022-06-10-impact.markdown', '2022-06-14-leadership.markdown',
          '2022-06-15-career-paths.markdown', '2022-07-06-doing-architecture.markdown', '2022-07-07-culture-map.md', '2022-07-08-six-simple-rules.md', '2022-07-09-storm.markdown', '2022-07-10-product-trap.md',
-         '2022-07-12-governance.markdown', '2022-07-15-economics.md', '2022-12-01-summary.markdown', '2022-12-24-bookshelf.markdown', '2022-12-24-tools.markdown']
+         '2022-07-12-governance.markdown', '2022-07-15-economics.md', '2022-12-01-summary.markdown']
 
 
 for post in posts:
