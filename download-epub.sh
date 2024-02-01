4ee7bf36c018d5ea558c7d097f5c9905f41a712c
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2024-02-01 19:15:57 +0100

    publishing changes

--- download-epub.sh
+++ download-epub.sh
@@ -1,9 +1,8 @@
 for (( ; ; )); do
-  curl "https://leanpub.com/groundedarchitecture/job_status?api_key=96FF3A08D7D0443AA6D0C64A73235444" >status,json
+  curl "https://leanpub.com/groundedarchitecture/job_status?api_key=96FF3A08D7D0443AA6D0C64A73235444" > status.json
   tail status.json
 
-  STRING="{}"
-  if grep -q "$STRING" "info.json"; then
+  if grep -o -e "{}" "status.json"; then
     echo "FOUND"
     curl -L "https://leanpub.com/s/B36725C24EB34CBD99A3FB1D71011E61.epub" -o assets/book/groundedarchitecture.epub
     exit
--- publish.sh
+++ publish.sh
@@ -6,4 +6,18 @@ git commit -m "publishing changes"
 git push
 
 curl -d "api_key=96FF3A08D7D0443AA6D0C64A73235444" https://leanpub.com/groundedarchitecture/preview.json
-curl "https://leanpub.com/groundedarchitecture.json?api_key=96FF3A08D7D0443AA6D0C64A73235444" > info.json
+
+for (( ; ; )); do
+  curl "https://leanpub.com/groundedarchitecture/job_status?api_key=96FF3A08D7D0443AA6D0C64A73235444" > status.json
+  tail status.json
+
+  if grep -o -e "{}" "status.json"; then
+    echo "FOUND"
+    curl -L "https://leanpub.com/s/B36725C24EB34CBD99A3FB1D71011E61.pdf" -o assets/book/groundedarchitecture.pdf
+    exit
+  else
+    echo 'the string does not exist'
+  fi
+  echo "sleeping 10 seconds..."
+  sleep 10
+done
