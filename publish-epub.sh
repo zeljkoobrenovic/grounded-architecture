11029a15aa6a215f57a99a8827990c4158ad1d8c
Zeljko Obrenovic <zeljkoobrenovic@yahoo.com>
2024-02-01 18:46:53 +0100

    publishing changes

--- publish-epub.sh
+++ publish-epub.sh
@@ -10,4 +10,4 @@ git push
 curl -d "api_key=96FF3A08D7D0443AA6D0C64A73235444" https://leanpub.com/groundedarchitecture/preview.json
 curl "https://leanpub.com/groundedarchitecture.json?api_key=96FF3A08D7D0443AA6D0C64A73235444" > info.json
 
-tail -n0 -f info.json | sed '/\{\}/ q'
+tail -n0 -f info.json | sed '/{}/ q'
